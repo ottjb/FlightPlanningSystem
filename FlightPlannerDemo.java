b92eb286ec832213767ff1f06e48ccbaac3a154c
ottjb <ottjb@usca.edu>
2024-02-22 00:01:47 -0500

    mhm yup

--- FlightPlannerDemo.java
+++ FlightPlannerDemo.java
@@ -1,30 +1,81 @@
 import java.awt.*;
 import javax.swing.*;
+import java.util.Random;
 
-public class FlightPlannerDemo {
+public class FlightPlannerDemo extends JFrame {
+
+    private static final int numOfAirports = 5;
+    private static final int screenWidth = 512;
+    private static final int screenHeight = 512;
+    private static final Random r = new Random();
+
+    private JPanel bestRoutePanel;
+    private JPanel currentRoutePanel;
+
+    public FlightPlannerDemo() {
+        setTitle("Flight Planner Demo");
+        setSize(screenWidth, screenHeight);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        AirportPoints[] airportPointsList = new AirportPoints[numOfAirports];
+        for (int i = 0; i < numOfAirports; i++) {
+            airportPointsList[i] = new AirportPoints(r.nextInt(screenWidth - 50) + 25, r.nextInt(screenHeight / 2 - 50) + 25);
+            airportPointsList[i].displayPoints();
+        }
+
+        bestRoutePanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.WHITE);
+                drawPoints(g, airportPointsList);
+                Graphics2D g2 = (Graphics2D) g.create();
+                g2.drawLine(0, 200, screenWidth, 200);
+
+            }
+        };
+
+        currentRoutePanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.WHITE);
+                drawPoints(g, airportPointsList);
+            }
+        };
+
+        getContentPane().setLayout(new GridLayout(2, 1));
+
+        bestRoutePanel.setBackground(Color.BLACK);
+        currentRoutePanel.setBackground(Color.BLACK);
+
+        getContentPane().add(bestRoutePanel);
+        getContentPane().add(currentRoutePanel);
+
+        setVisible(true);
+    }
+
+    private void drawPoints(Graphics g, AirportPoints[] airportPointsList) {
+        for (int i = 0; i < numOfAirports; i++) {
+            g.fillOval(airportPointsList[i].x, airportPointsList[i].y, 8, 8);
+        }
+    }
+
     public static void main(String[] args) {
-        int screenWidth = 300;
-        int screenHeight = 300;
-
-        DrawScreen frame = new DrawScreen();
-        frame.setSize(screenWidth, screenHeight);
-        frame.setLocation(512, 256);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Flight Planner Demo");
-        frame.setVisible(true);
+        SwingUtilities.invokeLater(FlightPlannerDemo::new);
     }
 }
 
-class DrawScreen extends JFrame {
-    public DrawScreen() {
-        setLayout(new GridLayout(2, 0));
-        JPanel bestRouteDisplay = new JPanel();
-        JPanel currentRouteDisplay = new JPanel();
+class AirportPoints {
+    int x;
+    int y;
 
-        bestRouteDisplay.setBackground(Color.BLACK);
-        currentRouteDisplay.setBackground(Color.BLUE);
+    public AirportPoints(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-        add(bestRouteDisplay);
-        add(currentRouteDisplay);
+    public void displayPoints() {
+        System.out.println("x=" + this.x + " y=" + this.y);
     }
 }
