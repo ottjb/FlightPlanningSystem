8763c2fa87690301fa2789bf1ce1216a2c8b4511
ottjb <ottjb@usca.edu>
2024-02-23 13:27:41 -0500

    mmm robots

--- FlightPlannerDemo.java
+++ FlightPlannerDemo.java
@@ -1,27 +1,34 @@
 import java.awt.*;
 import javax.swing.*;
+
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class FlightPlannerDemo extends JFrame {
 
-    private static final int numOfAirports = 5;
+    private static final int numOfAirports = 20;
     private static final int screenWidth = 512;
     private static final int screenHeight = 512;
     private static final Random r = new Random();
+    private static int generation = 0;
+    private static ArrayList<int[]> population = new ArrayList<int[]>();
 
     private JPanel bestRoutePanel;
-    private JPanel currentRoutePanel;
+    private static JPanel currentRoutePanel;
     private JPanel linePanel;
 
     public FlightPlannerDemo() {
         setTitle("Flight Planner Demo");
         setSize(screenWidth, screenHeight);
-        setLocation(512, 256);
+        setLocation(512, 128);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         AirportPoints[] airportPointsList = new AirportPoints[numOfAirports];
         for (int i = 0; i < numOfAirports; i++) {
-            airportPointsList[i] = new AirportPoints(r.nextInt(screenWidth - 50) + 25, r.nextInt(screenHeight / 2 - 50) + 25);
+            airportPointsList[i] = new AirportPoints(r.nextInt(screenWidth - 50) + 25,
+                    r.nextInt(screenHeight / 2 - 50) + 25);
         }
 
         bestRoutePanel = new JPanel() {
@@ -55,14 +62,35 @@ public class FlightPlannerDemo extends JFrame {
         getContentPane().add(currentRoutePanel);
 
         setVisible(true);
-        
-        
-        bestRoutePanel.add(createPathPanel(airportPointsList));
-        currentRoutePanel.add(createPathPanel(airportPointsList));
+
+        Path testPath = new Path();
+
+        for (int i = 0; i < numOfAirports; i++) {
+            testPath.addPath(airportPointsList[i]);
+        }
+
+        Path testPath2 = new Path();
+
+        testPath2.generatePath(numOfAirports, airportPointsList);
+
+        System.out.println("Distance: " + testPath.calculateDistance());
+        System.out.println("Fitness: " + testPath.calculateFitness());
+
+        bestRoutePanel.add(createPathPanel(testPath.path));
+
+        generation++;
+        Generation gen = new Generation(generation);
+        gen.generateRandomPopulation(numOfAirports, airportPointsList, 1000);
+        currentRoutePanel.add(createPathPanel(gen.highestFitnessPath.path));
+        gen.calcAverageFitness();
+
+        ArrayList<Path> topTenPercent = gen.getTopTenPercent();
+        for (int i = 0; i < topTenPercent.size(); i++) {
+            System.out.println((i + 1) + " Fitness: " + topTenPercent.get(i).calculateFitness());
+        }
 
         repaint();
     }
-
 
     private void drawPoints(Graphics g, AirportPoints[] airportPointsList) {
         for (int i = 0; i < numOfAirports; i++) {
@@ -94,14 +122,14 @@ public class FlightPlannerDemo extends JFrame {
         return panel;
     }
 
-    public static JPanel createPathPanel(AirportPoints[] airportPointsList) {
+    public static JPanel createPathPanel(ArrayList<AirportPoints> path) {
         JPanel panel = new JPanel(null) {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 g.setColor(Color.WHITE);
-                for(int i = 0; i < numOfAirports - 1; i++) {
-                    drawLeg(g, airportPointsList[i], airportPointsList[i + 1]);
+                for (int i = 0; i < path.size() - 1; i++) {
+                    drawLeg(g, path.get(i), path.get(i + 1));
                 }
             }
         };
@@ -125,5 +153,106 @@ class AirportPoints {
 
     public void displayPoints() {
         System.out.println("x=" + this.x + " y=" + this.y);
+    }
+}
+
+class Path {
+    ArrayList<AirportPoints> path = new ArrayList<>();
+    double distance;
+    double fitness;
+
+    public Path(AirportPoints path) {
+        this.path.add(path);
+        this.distance = 0;
+        this.fitness = 0;
+    }
+
+    public Path() {
+        this.path = new ArrayList<>();
+        this.distance = 0;
+        this.fitness = 0;
+    }
+
+    public void addPath(AirportPoints path) {
+        this.path.add(path);
+    }
+
+    public double calculateDistance() {
+        int totalDist = 0;
+        for (int i = 0; i < path.size() - 1; i++) {
+            int x1 = path.get(i).x;
+            int y1 = path.get(i).y;
+            int x2 = path.get(i + 1).x;
+            int y2 = path.get(i + 1).y;
+            totalDist += Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+            // System.out.println("Total Distance: " + totalDist);
+        }
+        distance = totalDist;
+        return distance;
+    }
+
+    public double calculateFitness() {
+        fitness = (100 / this.calculateDistance()) / path.size();
+        return fitness;
+    }
+
+    public void generatePath(int numOfAirports, AirportPoints[] airportPointsList) {
+        Random r = new Random();
+        ArrayList<AirportPoints> notVisited = new ArrayList<>(airportPointsList.length);
+        for (int i = 0; i < airportPointsList.length; i++) {
+            notVisited.add(airportPointsList[i]);
+        }
+        this.path.add(notVisited.get(0));
+        notVisited.remove(0);
+        while (this.path.get(this.path.size() - 1) != airportPointsList[numOfAirports - 1] && notVisited.size() > 0) {
+            int nextAirport = r.nextInt(notVisited.size());
+            this.path.add(notVisited.get(nextAirport));
+            notVisited.remove(nextAirport);
+        }
+    }
+}
+
+class Generation {
+    ArrayList<Path> population = new ArrayList<Path>();
+    int generation;
+    Path highestFitnessPath;
+
+    public Generation(int generation) {
+        this.generation = generation;
+    }
+
+    public void addPath(Path path) {
+        this.population.add(path);
+    }
+
+    public void generateRandomPopulation(int numOfAirports, AirportPoints[] airportPointsList, int size) {
+        for (int i = 0; i < size; i++) {
+            Path p = new Path();
+            p.generatePath(numOfAirports, airportPointsList);
+            p.calculateFitness();
+            population.add(p);
+            double highestFitness = 0;
+            if (p.calculateFitness() > highestFitness) {
+                highestFitness = p.calculateFitness();
+                highestFitnessPath = p;
+            }
+        }
+    }
+
+    public void calcAverageFitness() {
+        double totalFitness = 0;
+        for (int i = 0; i < population.size(); i++) {
+            totalFitness += population.get(i).calculateFitness();
+        }
+        System.out.println("Average Fitness: " + totalFitness / population.size());
+    }
+
+    public ArrayList<Path> getTopTenPercent() {
+        ArrayList<Path> topTenPercent = new ArrayList<Path>();
+        Collections.sort(population, Comparator.comparingDouble(Path::calculateFitness).reversed());
+        for (int i = 0; i < population.size() / 10; i++) {
+            topTenPercent.add(population.get(i));
+        }
+        return topTenPercent;
     }
 }
