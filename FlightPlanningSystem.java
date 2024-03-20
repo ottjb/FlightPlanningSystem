3631a17641a044c661fd488e0894ff83caff821e
ottjb <ottjb@usca.edu>
2024-03-20 17:13:54 -0400

    Meetig done 3/20/24

--- FlightPlanningSystem.java
+++ FlightPlanningSystem.java
@@ -1,46 +1,52 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class FlightPlanningSystem {
     public static void main(String[] args) {
         AirplaneManager airplaneManager = new AirplaneManager();
         AirportManager airportManager = new AirportManager();
+        FlightPlanningSystemGUI gui = new FlightPlanningSystemGUI();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setSize(800, 600);
+        gui.setVisible(true);
 
         System.out.println("Welcome to the Flight Planning System");
-        System.out.println("Created by: Jadon Ott, Mary-Alice Woolington, Brecken Merrill, and Cheyenne Kirby");
+        System.out.println("Created by: Jadon Ott, Mary Alice Woolington, Brecken Merrill, and Cheyenne Kirby");
         System.out.println("THIS SOFTWARE IS NOT TO BE USED FOR FLIGHT PLANNING OR NAVIGATIONAL PURPOSE");
 
-        boolean running = true;
-        int option;
-        Utility utility = new Utility();
-        while (running) {
-            System.out.println("--------------------------------");
-            System.out.println("Please select an option:");
-            System.out.println("1. Flight Planner");
-            System.out.println("2. Airplane Management");
-            System.out.println("3. Airport Management");
-            System.out.println("4. Exit");
-            option = utility.getIntegerInput();
-            System.out.println("--------------------------------");
-            System.out.println();
+        // boolean running = true;
+        // int option;
+        // Utility utility = new Utility();
+        // while (running) {
+        //     System.out.println("--------------------------------");
+        //     System.out.println("Please select an option:");
+        //     System.out.println("1. Flight Planner");
+        //     System.out.println("2. Airplane Management");
+        //     System.out.println("3. Airport Management");
+        //     System.out.println("4. Exit");
+        //     option = utility.getIntegerInput();
+        //     System.out.println("--------------------------------");
+        //     System.out.println();
 
-            switch (option) {
-                case 1:
-                    System.out.println("Flight Planner");
-                    break;
-                case 2:
-                    airplaneManager.displayMenu(utility);
-                    break;
-                case 3:
-                    airportManager.displayMenu();
-                    break;
-                case 4:
-                    running = false;
-                    utility.close();
-                    break;
-                default:
-                    System.out.println("Please select a valid option.");
-                    break;
-            }
-        }
+        //     switch (option) {
+        //         case 1:
+        //             System.out.println("Flight Planner");
+        //             break;
+        //         case 2:
+        //             airplaneManager.displayMenu(utility);
+        //             break;
+        //         case 3:
+        //             airportManager.displayMenu();
+        //             break;
+        //         case 4:
+        //             running = false;
+        //             utility.close();
+        //             break;
+        //         default:
+        //             System.out.println("Please select a valid option.");
+        //             break;
+        //     }
+        // }
     }
 
 }
